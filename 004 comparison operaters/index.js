fc1ff1062b457f07158adf5941fe60970ae1e8d4
Rasheed Ahmad <118961857+Rasheed-Ahmad07@users.noreply.github.com>
2024-01-08 21:27:27 +0500

    Add files via upload

---
+++ 004 comparison operaters/index.js
@@ -0,0 +1,12 @@
+// Comparison operaters
+// == , === , <= ,<= ,> ,< ,!= , !==
+
+// let x="45";
+// let y=5;
+// let z=x != y;
+// console.log(z);
+
+
+// let x="45";
+// let y="45";
+// console.log(x==y);
